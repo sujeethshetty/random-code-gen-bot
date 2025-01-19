f2e2bbefe7a98d2f3cfc0f156c26e63e12a72e78
Sujeeth Shetty <sujeeth.data@gmail.com>
2025-01-19 15:28:48 -0500

    Add random Python code

---
+++ random_code_1737318527.py
@@ -0,0 +1,24 @@
+Here is a simple Python program that generates a random number and asks the user to guess it. It includes functions, loops, and conditional statements:
+
+```python
+import random
+
+def generate_random_number():
+    return random.randint(1, 100)
+
+def guess_number():
+    random_number = generate_random_number()
+    while True:
+        user_guess = int(input("Guess the number (between 1 and 100): "))
+        if user_guess < random_number:
+            print("Too low, try again!")
+        elif user_guess > random_number:
+            print("Too high, try again!")
+        else:
+            print("Congratulations! You guessed the number correctly!")
+            break
+
+guess_number()
+```
+
+This program generates a random number between 1
