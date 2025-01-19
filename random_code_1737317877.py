ceb596e255deedbcb982bffb2dcb8841f7643d0d
Sujeeth Shetty <sujeeth.data@gmail.com>
2025-01-19 15:17:58 -0500

    Add random Python code

---
+++ random_code_1737317877.py
@@ -0,0 +1,16 @@
+Here is a simple Python program that generates a random quote each time it is run:
+
+```python
+import random
+
+# List of quotes
+quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
+    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
+]
+
+def generate_random_quote():
+    # Generate
