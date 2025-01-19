b6ab5960d2a6f81120b64870c61178679d25e5af
Sujeeth Shetty <sujeeth.data@gmail.com>
2025-01-19 15:23:47 -0500

    Add random Python code

---
+++ random_code_1737318226.py
@@ -0,0 +1,21 @@
+Here is a simple Python program that generates a random number between 1 and 10 and asks the user to guess the number. It then informs the user whether their guess was correct or not.
+
+```python
+import random
+
+def guess_number():
+    secret_number = random.randint(1, 10)
+    
+    while True:
+        user_guess = input("Guess a number between 1 and 10: ")
+        
+        if user_guess.isdigit():
+            user_guess = int(user_guess)
+            
+            if user_guess == secret_number:
+                print("Congratulations! You guessed the correct number.")
+                break
+            else:
+                print("Sorry, that's not the correct number. Try again.")
+        else:
+            print("Please enter a valid number
