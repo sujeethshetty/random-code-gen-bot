386ed211e4e3949e1140856cbcfaa6a071fa6c24
Sujeeth Shetty <sujeeth.data@gmail.com>
2025-01-19 15:20:55 -0500

    Add random Python code

---
+++ random_code_1737318055.py
@@ -0,0 +1,12 @@
+Here is a simple Python program that generates a random quote from a list of quotes each time it is run:
+
+```python
+import random
+
+# List of quotes
+quotes = [
+    "Do not take life too seriously. You will never get out of it alive. - Elbert Hubbard",
+    "In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Life is what happens when you're busy making other plans. - John Lennon",
+    "The best time to plant a tree was 20 years ago. The second best time is now. - Chinese
