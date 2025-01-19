e8c214186f6631b548ddbbde4252d35d143fb6f8
Sujeeth Shetty <sujeeth.data@gmail.com>
2025-01-19 15:19:40 -0500

    Add random Python code

---
+++ random_code_1737317979.py
@@ -0,0 +1,24 @@
+Sure! Here's a simple Python program that generates a random password for the user:
+
+```python
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def main():
+    print("Welcome to the Random Password Generator!")
+    length = int(input("Enter the length of the password you want to generate: "))
+    
+    if length <= 0:
+        print("Invalid length. Please enter a positive number.")
+    else:
+        password = generate_password(length)
+        print("Your random password is: ", password)
+
+if __name__ == "__main__":
+    main()
+``
